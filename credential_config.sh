f0e6ee3f9e9bd0837e9be7eadb93c96b11d7c10a
Rajesh <rajeshnadarajan2003@gmail.com>
2025-07-07 22:25:18 +0530

    Add files via upload

---
+++ credential_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Database Configuration
+DB_USER="DB_USER_NAME" #eg:gershon
+DB_PASS="YOUR_PASS"
+DB_NAME="YOUR_DB_NAME"  # eg:gitea
+DB_HOST="YOUR_HOST" #eg:localhost
+
+# Gitea Configuration
+GITEA_DATA_DIR="YOUR_GITEA_REPOS"  # eg :/var/lib/gitea
+
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID="YOUR_AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY="YOUR_AWS_SECRET_ACCESS_KEY"
+AWS_DEFAULT_REGION="YOUR_REGION" #eg:ap-south-1
+S3_BUCKET="YOUR_S3_BUCKET_NAME"
+S3_PATH="" #its not nessasary to fill this option
+
+
